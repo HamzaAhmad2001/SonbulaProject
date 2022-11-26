34058741424aff94fa4981ed82721cb8a9af65a9
alarrabee <hamza.ibrahem@hotmail.com>
2022-11-26 20:12:22 +0300

    this is second commit

--- app/src/main/java/com/example/sonbulaapp/MainActivity.java
+++ app/src/main/java/com/example/sonbulaapp/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.sonbulaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button btn;
+        Button btn2;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=(Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
+
+        btn2=(Button)findViewById(R.id.button2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity()
+    {
+        Intent intent = new Intent(this,EditProfile.class);
+        startActivity(intent);
+    }
+    public void openActivity2()
+    {
+        Intent intent = new Intent(this,ProfileVolunteer.class);
+        startActivity(intent);
     }
 }
