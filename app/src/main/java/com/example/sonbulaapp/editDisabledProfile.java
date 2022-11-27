68ab1c417e5daa6456a70d2ae4949361c7d73cc5
hayahas <103423497+hayahas@users.noreply.github.com>
2022-11-27 21:03:06 +0300

    New commit From Haya

---
+++ app/src/main/java/com/example/sonbulaapp/editDisabledProfile.java
@@ -0,0 +1,14 @@
+package com.haya.sonbulaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editDisabledProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_disabled_profile);
+    }
+}
---
+++ app/src/main/java/com/example/sonbulaapp/instant_help.java
@@ -0,0 +1,14 @@
+package com.haya.sonbulaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class instant_help extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instant_help);
+    }
+}
